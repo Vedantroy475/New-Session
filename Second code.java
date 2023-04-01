c01f9fb8a9a205ee58f2209d7fa62f80ca155199
Vedantroy475 <vedantroy3@gmail.com>
2023-04-01 23:42:42 +0530

    Second got committed by using branch pwAla

---
+++ Second code.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class M{
+	public static void main (String[] args){
+
+		Scanner read = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number =read.nextInt();
+		System.out.println("You entered " + number);
+	}
+
+}
+//Some code you may not
